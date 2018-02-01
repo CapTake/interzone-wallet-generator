0fc2e11e47def8ebbe468e12557b8b10d065067f
Boris <boma@nm.ru>
2018-02-01 17:41:47 +0300

    qrcode test

--- src/components/header.vue
+++ src/components/header.vue
@@ -19,6 +19,7 @@
       <div class="tc-l mt4 mt5-m mt6-l ph3">
         <h1 class="f2 f1-l fw2 white-90 mb0 lh-title">This is your super impressive headline</h1>
         <h2 class="fw1 f3 white-80 mt3 mb4">Now a subheadline where explain your wonderful new startup even more</h2>
+        <img :src="qrcode" />
         <a class="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/">Call to Action</a>
         <span class="dib v-mid ph3 white-70 mb3">or</span>
         <a class="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="">Secondary call to action</a>
@@ -30,8 +31,22 @@
 
 <script>
 import logo from '../assets/logo.png'
+var QRCode = require('qrcode')
 export default {
-  logo
+  logo,
+  data () {
+    return {
+      qrcode: ''
+    }
+  },
+  created () {
+    QRCode.toDataURL('2we5e56det572d5f7dcxgcv76552we').then(url => {
+      this.qrcode = url
+      console.log(url)
+    }).catch(err => {
+      console.error(err)
+    })
+  }
 }
 </script>
 
