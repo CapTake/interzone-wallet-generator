327a9b8dc98ad8071d4d7cc6d18c53a6337fc1d0
Boris <boma@nm.ru>
2018-02-06 09:50:16 +0300

    BUGFIX: wallet QR codes weren't updated

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -3,12 +3,12 @@
   <div class="cf">
     <div class="fl w-100 w-50-ns pv4">
       <span class="b">Public address</span> <span class="bg-green pl1 pr1 white">OK to share</span>
-      <div><img :src="pubq" class="mw-60 mv-80-ns mh-60 mh-80-ns center w5 h5" alt="Public address, you may share it" /></div>
+      <div><img :src="pubq" class="mw-60 mv-80-ns mh-60 mh-80-ns center w5 h5" alt="Public address, you may share it" :key="pub" /></div>
       <div class="fw4 f7 ph2 code" style="word-break: break-all; word-wrap: break-word;">{{ pub }}</div>
     </div>
     <div class="fl w-100 w-50-ns pv4">
       <span class="b">Private key</span> <span class="bg-red pl1 pr1 white">KEEP SECRET!</span>
-      <div><img :src="privq" class="mw-60 mv-80-ns  mh-60 mh-80-ns center w5 h5" alt="Private key, keep it safe! Anyone who has it controls your money!" /></div>
+      <div><img :src="privq" class="mw-60 mv-80-ns  mh-60 mh-80-ns center w5 h5" alt="Private key, keep it safe! Anyone who has it controls your money!" :key="priv" /></div>
       <div class="fw4 f7 ph2 code" style="word-break: break-all; word-wrap: break-word;">{{ priv }}</div>
     </div>
   </div>
@@ -26,17 +26,25 @@ export default {
       step: 0
     }
   },
+  methods: {
+    onUpdate () {
+      QRCode.toDataURL(this.pub).then(url => {
+        this.pubq = url
+      }).catch(err => {
+        console.error(err.message)
+      })
+      QRCode.toDataURL(this.priv).then(url => {
+        this.privq = url
+      }).catch(err => {
+        console.error(err.message)
+      })
+    }
+  },
   created () {
-    QRCode.toDataURL(this.pub).then(url => {
-      this.pubq = url
-    }).catch(err => {
-      console.error(err.message)
-    })
-    QRCode.toDataURL(this.priv).then(url => {
-      this.privq = url
-    }).catch(err => {
-      console.error(err.message)
-    })
+    this.onUpdate()
+  },
+  updated () {
+    this.onUpdate()
   }
 }
 </script>
